1f294ae6a2aab3224b73789dcdb2e8d9dc5e0841
Muhammad Yaseen <89628668+mhmdyaseen@users.noreply.github.com>
2025-07-05 17:12:12 +0530

    Delete WebAgent directory

--- WebAgent/src/agent/__init__.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC,abstractmethod
-
-class BaseAgent(ABC):
-    @abstractmethod
-    def invoke(self,input:str):
-        pass
-    @abstractmethod
-    def stream(self,input:str):
-        pass
